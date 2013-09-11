c1d9ca89bec186322adcf7950211dfb405c30b39
jb <bodysplash@gmail.com>
2013-09-11 08:00:06 +0200

    déplacement des propositions dans les listes

--- public/app/pages/sondage/vote_controleur.js
+++ public/app/pages/sondage/vote_controleur.js
@@ -3,7 +3,19 @@
 
     angular.module("pages.sondage")
         .controller('VoteControleur', ['$scope', 'PropositionsSondageRessource', function($scope, Propositions) {
+
+            $scope.vote =[];
             $scope.$on("SondageChargé", chargePropositions);
+
+            $scope.votePour = function(proposition) {
+              $scope.propositions = _.without($scope.propositions, proposition);
+              $scope.vote.push(proposition);
+            };
+
+            $scope.annuleVotePour = function(proposition) {
+                $scope.propositions.push(proposition);
+                $scope.vote = _.without($scope.vote, proposition);
+            };
 
             function chargePropositions() {
                 $scope.propositions = Propositions.query({id: $scope.sondage.id});
