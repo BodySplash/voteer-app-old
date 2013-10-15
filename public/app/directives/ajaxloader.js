ebc0da424944a4763383fb1c677a8119f82be372
jb <bodysplash@gmail.com>
2013-10-15 18:13:28 +0200

    correction soucis de concaténation js

--- public/app/directives/ajaxloader.js
+++ public/app/directives/ajaxloader.js
@@ -1,12 +1,13 @@
-(function(angular) {
-    'use strict';
-    angular.module('ngVoteer', [])
+(function (angular) {
+    "use strict";
+
+    angular.module("ngVoteer")
         .directive("loader", function () {
             return {
                 restrict: 'AC',
                 replace: true,
                 scope: { loader: '=' },
-                template: '<div ng-hide="loader" class="loading"><h3><span class="label label-info">Loading...</span></h3>'
+                template: '<div ng-hide="loader" class="loading"><h3><span class="label label-info">Loading...</span></h3></div>'
             };
         });
 })(angular);
--- public/app/directives/loading.js
+++ public/app/directives/loading.js
@@ -19,4 +19,4 @@
                 }
             };
         });
-})(angular)
+})(angular);
