5c69fae3624f88e3cc0252fd056d1d0776c2ebaa
Charles Couillard <couillardcharles@gmail.com>
2013-12-11 16:21:51 +0100

    début de la traduction de l'interface

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 var express = require("express"),
-	path = require('path');
+	path = require('path'),
+    i18n = require('i18next');
 
 
 var app = express();
 
 console.log("Configuring app for " + app.get('env') + " environment");
 
+i18n.init({
+    saveMissing: true,
+    ignoreRoutes: ['img/','assets/', 'font/'],
+    fallbackLng : 'en'
+});
 
 app.configure(function() {
-	app.set('views', path.join(__dirname, '/views'));
-	app.set('view engine', 'jade');
-	app.use(express.logger());
+    app.use(express.logger());
     app.use(express.cookieParser());
     app.use(express.bodyParser());
+    app.use(i18n.handle);
+    app.set('views', path.join(__dirname, '/views'));
+	app.set('view engine', 'jade');
     app.use(express.methodOverride());
     app.use(express.static(__dirname + '/public/'));
     app.use(express.errorHandler());
-
 });
 
 app.configure('development', function() {
@@ -34,6 +40,8 @@ app.configure('production', function() {
 
 require('./assets')(app);
 require('./routes')(app);
+
+i18n.registerAppHelper(app);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
